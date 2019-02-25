ff7cfc3e26d873ba4f1b91105a0335c32c758ddb
Peter oyebamiji <peter2olumide@gmail.com>
2019-02-25 19:06:10 +0100

    imported bodyParser into index.js file

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
+import bodyParser from 'body-parser';
+
 import express from 'express';
 
 
 const app = express();
 const PORT = 8000;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     return res.send('The API is working');
