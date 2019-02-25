0024d94bad4a6308ed4b39c6e22c64ca2d1a245a
Peter oyebamiji <peter2olumide@gmail.com>
2019-02-25 18:59:56 +0100

    added mealController

---
+++ api/controllers/meal.controller.js
@@ -0,0 +1,31 @@
+import MealService from '../services/meal.service';
+
+const MealController = {
+    fetchAllMeals(req, res) {
+        const allMeals = MealService.fetchAllMeals();
+        return res.json({
+            status: 'success',
+            data: allMeals
+        }).status(200);
+    },
+
+    addAMeal(req, res) {
+        
+        const newMeal = req.body;
+        const createdMeal = MealService.addMeal(newMeal);
+        return res.json({
+            status: 'success',
+            data: createdMeal
+        }).status(201);
+    },
+    getSingleMeal(req, res) {
+        const id = req.params.id;
+        const foundMeal = MealService.getAMeal(id);
+        return res.json({
+            status: 'success',
+            data: foundMeal
+        }).status(201);
+    }
+};
+
+export default MealController;
